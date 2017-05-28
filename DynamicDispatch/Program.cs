eb34c40411399769d23941b1364b1cb0fe3639d8
Alex Henderson <alex@devdefined.com>
2017-05-28 13:57:31 +1200

    First experiment - confirm that dynamic dispatch in a netstandard16 library does work.

---
+++ DynamicDispatch/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DynamicDispatchLibrary;
+
+namespace DynamicDispatch
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			DynamicPrinter.Print(1);
+			DynamicPrinter.Print("test");
+			DynamicPrinter.Print(new { page = 1, pageSize = 20});
+			Console.ReadLine();
+		}
+	}
+}
---
+++ DynamicDispatchLibrary/DynamicPrinter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DynamicDispatchLibrary
+{
+    public class DynamicPrinter
+    {
+	    public static void Print(object item)
+	    {
+		    PrintMe((dynamic)item);
+	    }
+
+	    static void PrintMe(int i)
+	    {
+		    Console.WriteLine($"integer: {i}");
+	    }
+
+	    static void PrintMe(string s)
+		{
+		    Console.WriteLine($"string: {s}");
+	    }
+
+	    static void PrintMe(object o)
+	    {
+		    Console.WriteLine($"object: {o ?? "<null>"}");
+	    }
+	}
+}
